e812e5ff55267b6b4f325685ba40014eb386a4ac
mayang542799675 <542799675@qq.com>
2021-05-26 17:09:35 +0800

    2021.5.26

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -12,13 +12,15 @@ module.exports = [
     text: '小窝',
     link: '/family/',  //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
     items: [
-      {text: 'my', items: [
-        {text: 'test1', link: '/pages/c8f128/'}, // 这些link是在相应md文件定义的永久链接。另外，注意结尾是有斜杠的
-      ]},
-      {text: 'mmq', items:[
-        {text: 'test2', link: '/pages/9f15c1a281d8bedb/'},
-
-      ]}
+        {text: '吴忠小窝', link: '/pages/20210526/'},
+        {text: '安康小窝', link: '/pages/202105261/'}
+      // {text: 'my', items: [
+      //   {text: 'test1',link: '/family/'}, // 这些link是在相应md文件定义的永久链接。另外，注意结尾是有斜杠的
+      // ]},
+      // {text: 'mmq', items:[
+      //   {text: 'test2', link: '/pages/9f15c1a281d8bedb/'},
+      //
+      // ]}
     ]
   },
   {
